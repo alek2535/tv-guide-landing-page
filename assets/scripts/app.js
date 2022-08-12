608ff70eaf8cab71d63d9402b35d87eb76849e34
Alek Valencia <alekvalencia2535@gmail.com>
2022-08-11 19:05:00 -0700

    added media queries and removed some css styling to make page more responsive. added responsive features to slider. removed moment.js and used Date object to get tomorrow's date to minimize added packages. also added comments to better understand and structure code.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,63 @@
 $(document).ready(function() {
-    let tomorrow = moment().add(1,'days').format('YYYY-MM-DD');
+    // LOGIC TO DYNAMICALLY GET TOMORROW'S DATE
+    let date = new Date();
+    date.setDate(date.getDate() + 1);
+    date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+    let tomorrow = date.toISOString().slice(0, 10);
 
+    // AJAX CALL TO API FOR TOMORROW'S TV SHOW SCHEDULE
     $.ajax({
         url: `https://api.tvmaze.com/schedule?country=US&date=${tomorrow}`,
         method: 'GET',
     })
     .then(function(response) {
-        console.log(response);
-        
+        // LOGIC TO DYNAMICALLY CREATE TV SHOW SCHEDULE
         for (let i = 0; i < 24; i++) {
-            $(`<img src=${response[i].show.image.medium} class="slider-image" />`).appendTo('#show-slider');
-        }
+            $(`<img src=${response[i].show.image.medium} alt=${response[i].show.name} class="slider-image responsive" />`).appendTo('#show-slider');
+        };
 
+        // LOGIC TO CREATE SLIDER
         $('#show-slider').slick({
             infinite: false,
             slidesToShow: 6,
             slidesToScroll: 6,
+            responsive: [
+                {
+                    breakpoint: 1000,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5,
+                    }
+                },
+                {
+                    breakpoint: 900,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                    }
+                },
+                {
+                    breakpoint: 700,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+                {
+                    breakpoint: 300,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
         });
-    })
+    });
 });
